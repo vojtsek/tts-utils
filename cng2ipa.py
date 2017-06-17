1dbb6d432c73a1e0d618249f968dbd99f1741400
vojtsek <vojta.hudecek@gmail.com>
2017-06-17 18:56:10 +0200

    added FSTs

--- cng2ipa.py
+++ cng2ipa.py
@@ -16,50 +16,45 @@ def ignore(char):
 
 
 cng2ipa = {}
-<<<<<<< HEAD
-mapfile = 'cng2ipa.tsv'
-if len(sys.argv) > 2:
-    mapfile = sys.argv[2]
-with open(mapfile, 'r') as f:
-    for line in f:
-        sp = line.strip().split('\t')
-        cng2ipa[sp[0]] = sp[1]
-=======
+#mapfile = 'cng2ipa.tsv'
+#if len(sys.argv) > 2:
+#    mapfile = sys.argv[2]
+#with open(mapfile, 'r') as f:
+#    for line in f:
+#        sp = line.strip().split('\t')
+#        cng2ipa[sp[0]] = sp[1]
 with open(sys.argv[2], 'r') as f:
     for line in f:
         sp = line.split('\t')
         cng2ipa[sp[0]] = sp[1].strip()
->>>>>>> 6de27918b42d74c905c67eebf0a5c7c0597fdcbb
 
 trnfile = sys.argv[1]
 content = ''
 with open(trnfile, 'r') as f:
-<<<<<<< HEAD
-    content = f.read()
-trn = content.split('\n')[0]
-for trn in content.split('\n'):
-    pos = 0
-    ipatrn = ''
-    while pos < len(trn):
-        char = trn[pos:pos+2]
-        if char in cng2ipa:
-            ipatrn += cng2ipa[char] + ' '
-            pos += 2
-        else:
-            char = trn[pos]
-            pos += 1
-            if ignore(char):
-                continue
-            elif char == ' ':
-                #ipatrn += ' SIL '
-                pass
-            else:
-                ipatrn += cng2ipa[char] + ' '
-
-    with open(trnfile + '.ipa', 'a') as f:
-        f.write(ipatrn + '\n')
-    #create_ssml(ipatrn, f)
-=======
+#    content = f.read()
+#trn = content.split('\n')[0]
+#for trn in content.split('\n'):
+#    pos = 0
+#    ipatrn = ''
+#    while pos < len(trn):
+#        char = trn[pos:pos+2]
+#        if char in cng2ipa:
+#            ipatrn += cng2ipa[char] + ' '
+#            pos += 2
+#        else:
+#            char = trn[pos]
+#            pos += 1
+#            if ignore(char):
+#                continue
+#            elif char == ' ':
+#                #ipatrn += ' SIL '
+#                pass
+#            else:
+#                ipatrn += cng2ipa[char] + ' '
+#
+#    with open(trnfile + '.ipa', 'a') as f:
+#        f.write(ipatrn + '\n')
+#    #create_ssml(ipatrn, f)
     for line in f:
         line = line.strip()
         if len(line) < 1:
@@ -67,4 +62,3 @@ for trn in content.split('\n'):
         for ph in line.split():
             sys.stdout.write(cng2ipa[ph.strip('012')] + ' ')
         sys.stdout.write('\n')
->>>>>>> 6de27918b42d74c905c67eebf0a5c7c0597fdcbb
--- trn_correlation.py
+++ trn_correlation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from correlation import correlation
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
@@ -8,4 +9,12 @@ labels = pd.read_csv(sys.argv[2], header=None)
 lbls = []
 for i in labels.values:
     lbls.append(np.mean([int(a) for a in i[1:]]))
-print(correlation(data.values[:,1], lbls))
+d = data.values[:,1]
+#print(correlation(d, lbls))
+
+plt.hist(d, bins=30, color='lightblue')
+plt.xlabel('M2 measure', fontsize=18)
+plt.ylabel('Number of recordings', fontsize=18)
+#plt.plot(np.arange(d.shape[0]), sorted(d), linewidth=3)
+plt.savefig('m2distr.jpg')
+
